376db80425c1f04aa62b398bd507bac8c5565e5c
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2014-02-13 08:21:02 +1100

    Preload images

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,24 @@ bespoke.from('article', {
   progress: true,
   state: true
 });
+
+(function preloadBackgroundImages() {
+
+  var matches, image,
+    forEach = function(arrayLike, fn) {
+      [].slice.call(arrayLike, 0).forEach(fn);
+    };
+
+  forEach(document.styleSheets, function(sheet) {
+    forEach(sheet.rules, function(rule) {
+      if (rule.style && rule.style.backgroundImage) {
+        matches = rule.style.backgroundImage.match(/url\((.*)\)/);
+        if (matches) {
+          image = new Image();
+          image.src = matches[1];
+        }
+      }
+    });
+  });
+
+}());
